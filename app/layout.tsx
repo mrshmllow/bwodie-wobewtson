ab6de895e4538ad821816da31c6da7ef07c37e73
marshmallow <marshycity@gmail.com>
2023-04-28 21:25:54 +1000

    misc: add analytics bc why not

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${inter.className} bg-slate-800`}>{children}</body>
+      <Analytics />
     </html>
   );
 }
