32b48a7c06979772a538801fbf83be8db1582d67
marshmallow <marshycity@gmail.com>
2023-04-28 21:17:55 +1000

    style: prettier-sweep

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,45 @@
-import "server-only"
+import "server-only";
 
 interface Resource {
   title: string;
   url: string;
 }
 
-async function getDescription(videoId: string): Promise<{ descriptionParagraph: string; resources: Resource[] }> {
-  const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${process.env.API_KEY}`, {
-    next: {
-      revalidate: false
+async function getDescription(
+  videoId: string
+): Promise<{ descriptionParagraph: string; resources: Resource[] }> {
+  const response = await fetch(
+    `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${process.env.API_KEY}`,
+    {
+      next: {
+        revalidate: false,
+      },
     }
-  });
-  const description = await response.json() as {
+  );
+  const description = (await response.json()) as {
     items: {
       snippet: {
-        description: string
-      }
-    }[]
-  }
+        description: string;
+      };
+    }[];
+  };
 
-  const lines = description.items[0].snippet.description.split('\n');
+  const lines = description.items[0].snippet.description.split("\n");
   const resources: Resource[] = [];
-  let descriptionParagraph = '';
+  let descriptionParagraph = "";
   let resourcesStart = false;
 
   for (const line of lines) {
-    if (!descriptionParagraph && line.trim() !== '') {
+    if (!descriptionParagraph && line.trim() !== "") {
       descriptionParagraph = line;
     }
 
-    if (line.trim() === '==========Resources==========') {
+    if (line.trim() === "==========Resources==========") {
       resourcesStart = true;
       continue;
     }
 
-    if (resourcesStart && line.trim() !== '') {
+    if (resourcesStart && line.trim() !== "") {
       const resourceInfo = line.split(/:(.+)/);
       resources.push({
         title: resourceInfo[0].trim(),
@@ -42,7 +47,7 @@ async function getDescription(videoId: string): Promise<{ descriptionParagraph: 
       });
     }
 
-    if (resourcesStart && line.trim() === '') {
+    if (resourcesStart && line.trim() === "") {
       break;
     }
   }
@@ -54,29 +59,34 @@ async function getDescription(videoId: string): Promise<{ descriptionParagraph: 
 }
 
 async function getData() {
-  const res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=UCld68syR8Wi-GY_n4CaoJGA&maxResults=20&order=date&type=video&key=${process.env.API_KEY}`, {
-    next: {
-      revalidate: 60
+  const res = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=UCld68syR8Wi-GY_n4CaoJGA&maxResults=20&order=date&type=video&key=${process.env.API_KEY}`,
+    {
+      next: {
+        revalidate: 60,
+      },
     }
-  });
+  );
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
 
-  const data = await res.json() as {
+  const data = (await res.json()) as {
     items: {
       id: {
-        videoId: string
-      },
+        videoId: string;
+      };
       snippet: {
-        title: string,
-        publishTime: string
-      }
-    }[]
+        title: string;
+        publishTime: string;
+      };
+    }[];
   };
 
-  const videoDetailsPromises = data.items.map((item) => getDescription(item.id.videoId));
+  const videoDetailsPromises = data.items.map((item) =>
+    getDescription(item.id.videoId)
+  );
 
   const videoDetails = await Promise.all(videoDetailsPromises);
 
@@ -85,13 +95,13 @@ async function getData() {
       id: item.id.videoId,
       title: item.snippet.title,
       publishTime: item.snippet.publishTime,
-      ...videoDetails[index]
+      ...videoDetails[index],
     };
   });
 }
 
 export default async function Home() {
-  const data = await getData()
+  const data = await getData();
 
   return (
     <main className="md:max-w-3xl mx-auto">
@@ -103,18 +113,32 @@ export default async function Home() {
         <p className="text-slate-300">this is a joke.</p>
         <p className="text-slate-300 line-through">but not really</p>
 
-        {data.map(video =>
-          <div key={video.id} className="rounded-md bg-slate-700 border-slate-700 border p-4">
-            <p className="text-lg font-bold text-slate-300 pb-2">{video.title}</p>
+        {data.map((video) => (
+          <div
+            key={video.id}
+            className="rounded-md bg-slate-700 border-slate-700 border p-4"
+          >
+            <p className="text-lg font-bold text-slate-300 pb-2">
+              {video.title}
+            </p>
 
             <p className="text-slate-400 pb-2">{video.descriptionParagraph}</p>
 
             <ul>
-              {video.resources.map((resource, index) => <li key={index} className="before:content-['-'] before:absolute before:left-0 before:text-slate-500 relative pl-[1.2em]"><a href={resource.url} className="text-blue-400">{resource.title}</a></li>)}
+              {video.resources.map((resource, index) => (
+                <li
+                  key={index}
+                  className="before:content-['-'] before:absolute before:left-0 before:text-slate-500 relative pl-[1.2em]"
+                >
+                  <a href={resource.url} className="text-blue-400">
+                    {resource.title}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
-        )}
+        ))}
       </div>
     </main>
-  )
+  );
 }
